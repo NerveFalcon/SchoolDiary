f1f6b1d4744737cc7e3d35b2a7b08bb95ba80c1c
NerveFalcon <suraevoleg@gmail.com>
2024-12-13 08:33:44 +0300

    Add Calendar page

---
+++ HomeWork/HomeWork/Calendar/Models/DayModel.cs
@@ -0,0 +1,19 @@
+namespace HomeWork.Calendar.Models;
+
+public class DayModel(int dayOfMonth)
+{
+	private readonly DateTime _date = new(DateTime.Today.Year, DateTime.Today.Month, dayOfMonth);
+
+	public DayOfWeekRu DayOfWeek => Translate(_date.DayOfWeek);
+	public int Day => _date.Day;
+	public int Month => _date.Month;
+	public int Year => _date.Year;
+	public int DayOfYear => _date.DayOfYear;
+
+	private static DayOfWeekRu Translate(DayOfWeek dayOfWeek)
+	{
+		if (dayOfWeek is System.DayOfWeek.Sunday) return DayOfWeekRu.Sunday;
+
+		return (DayOfWeekRu)dayOfWeek;
+	}
+}
